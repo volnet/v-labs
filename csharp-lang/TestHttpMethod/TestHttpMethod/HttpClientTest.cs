69901e256e937e784d21aae88b5c1c4f7eb0b433
Cen Gong <Cen Gong>
2017-02-26 23:08:54 +0800

    add Test HttpGet to https resources

---
+++ csharp-lang/TestHttpMethod/TestHttpMethod/HttpClientTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHttpMethod
+{
+    public class HttpClientTest
+    {
+        public static async Task<string> HttpGet(string uri)
+        {
+            HttpClient client = new HttpClient();
+            return await client.GetStringAsync(uri);
+        }
+    }
+}
