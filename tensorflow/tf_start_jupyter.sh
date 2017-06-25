1cc617ec7340949700f8a818d1c6f0571fcd4635
volnet <volnet@tom.com>
2017-06-26 00:51:10 +0800

    modify tf_start_jupyter.sh & rollback Dockerfile for juputer

--- tensorflow/tf_start_jupyter.sh
+++ tensorflow/tf_start_jupyter.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8888:8888 -v $(pwd)/notebooks:/notebooks -v$(pwd)/logs:/logs --name my-tf-notebook volnet/tf-notebook
+docker run -d -p 8888:8888 -v $(pwd)/notebooks:/notebooks -v $(pwd)/logs:/logs --name my-tf-notebook volnet/tf-notebook
