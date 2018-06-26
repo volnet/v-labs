c96b445c2d32139d87a8e9802ca7782cc66c19be
root <root@CentOS.localdomain>
2018-06-26 12:11:01 -0400

    add elastic project

---
+++ elastic/scripts/run-es.sh
@@ -0,0 +1,9 @@
+docker rm -f elas
+
+docker rm -f mykibana
+
+docker run --name elas -p 9200:9200 -p 9300:9300 -d -v esdata:/usr/share/elasticsearch/data elasticsearch
+
+docker run --name mykibana -p 5601:5601 -d --link elas:elasticsearch kibana 
+
+docker ps -a
