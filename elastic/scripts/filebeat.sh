96db1b086e6635fcbad107b153ec72bbad9f3fb2
volnet <volnet@tom.com>
2018-06-30 00:40:13 +0800

    add comment for shell file

--- elastic/scripts/filebeat.sh
+++ elastic/scripts/filebeat.sh
@@ -1,3 +1,5 @@
+echo writer -> logs -> filebeat -> elasticsearch -> kibana
+
 docker rm -f filebeat_test
 
 docker run -d --net host --name filebeat_test -v "$PWD/../filebeat/configs/filebeat.yml":/usr/share/filebeat/filebeat.yml:z -v "$PWD/../filebeat/configs/modules.d/":/usr/share/filebeat/modules.d/:z -v "$PWD/../filebeat/logs/":/usr/share/filebeat/logs/:z docker.elastic.co/beats/filebeat:6.3.0
